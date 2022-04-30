a83167e540ffe3bcc871a96136a0c8253f6d4ea1
sammie-lee <104579825+sammie-lee@users.noreply.github.com>
2022-04-30 18:02:24 +0100

    Add files via upload

---
+++ JS main.js
@@ -0,0 +1,26 @@
+//string,numbers,Boolean,null, undefined
+
+const name='John'
+const age= 30
+const rating=4.5
+const isCool=true
+const x =null
+const y= undefined
+let z;
+
+console.log(typeof)
+
+const name='john'
+const age= 30
+//contructor function
+function Person(firstName, lastName, dob){
+    this.firstname = firstName;
+    this.lastName = lastName;
+    this.dob = dob;
+    this.get}
+
+//Instantiate object
+const person1= new Person('John', 'Doe', '4-3-1980');
+const Person2= new Person('Mary', 'Smith', '3-6-1970')
+
+console.log(person2.firstName);
